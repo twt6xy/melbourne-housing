3b20118e732e19d808fea7bcbab8562707588def
twt6xy <twt6xy@virginia.edu>
2021-04-24 19:49:56 -0400

    cleaned the dataset and set up a separate script to run analysis on cleaned dataset

---
+++ data-cleaning.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+
+# read in the raw data
+melbourne <- read.csv("melb_data_raw.csv", header=TRUE)
+
+# view if any null columns
+colSums(is.na(melbourne))
+
+# =================================================================================================================
+# BuildingArea: 6417 NA Values - fillna with median building area for type of building with same number of bedrooms
+# YearBuilt: 5344 NA Values - fillna with the median year built for type of building in the respective suburb
+# REASONING: Filling NA values strictly by suburb or number of bedrooms would introduce flawed assumptions,
+#            an old suburb that used to consist of mainly houses may be be home to new builds that would
+#            consist of townhomes and apartments, so we need to consider building type during the imputations.
+# Car: 62 NA Values - fillna with with mode of Car since it's only 62 instances
+# =================================================================================================================
+
+# getting the median building area by number of rooms and type of building
+roomsAndTypeGrouped <- melbourne %>%
+        group_by(Rooms, Type) %>%
+        summarise(medianArea = median(BuildingArea, na.rm=TRUE))
+
+# getting the median year built by suburb and type of building 
+suburbAndTypeGrouped <- melbourne %>%
+        group_by(Suburb, Type) %>%
+        summarise(medianYear = median(YearBuilt, na.rm=TRUE))
+
+# getting the median number of car spots per building type to fill the 62 null values with
+TypeGrouped <- melbourne %>%
+        group_by(Type) %>%
+        summarise(medianCar = median(Car, na.rm=TRUE))
+
+# After grouping median year built by suburb and type of building, there are still 80 NA values, so I'll fill those by median year by building type
+TypeGroupForRemainingNAyears <- melbourne %>%
+        group_by(Type) %>%
+        summarise(medianYearType = median(YearBuilt, na.rm=TRUE))
+
+# filling in the blank council areas
+suburbAndCouncilGrouped <- melbourne %>%
+        group_by(Suburb, CouncilArea)
+
+# median for 4 rooms of type U = 171.5, so I'm going to double it for the null value at 8 rooms of type u.
+roomsAndTypeGrouped$medianArea[19]=394.45
+
+# median for 5 rooms of type h = 260, so I'm doubling it for the null value at 10 rooms of type h
+roomsAndTypeGrouped$medianArea[20]=520
+
+# merging all of the grouped datasets into a copy of the melbourne data, and then filling null values with its respective median value
+testMerge <- merge(melbourne, roomsAndTypeGrouped, by=c("Rooms", "Type"))
+testMerge$BuildingArea <- ifelse(is.na(testMerge$BuildingArea), testMerge$medianArea, testMerge$BuildingArea)
+testMerge <- merge(testMerge, suburbAndTypeGrouped, by=c("Suburb", "Type"))
+testMerge$YearBuilt <- ifelse(is.na(testMerge$YearBuilt), testMerge$medianYear, testMerge$YearBuilt)
+testMerge <- merge(testMerge, TypeGrouped, by="Type")
+testMerge$Car <- ifelse(is.na(testMerge$Car), testMerge$medianCar, testMerge$Car)
+testMerge <- merge(testMerge, TypeGroupForRemainingNAyears, by="Type")
+testMerge$YearBuilt <- ifelse(is.na(testMerge$YearBuilt), testMerge$medianYearType, testMerge$YearBuilt)
+
+# drop the median columns. Address, seller, and method of sale don't have much to do with the value of a house
+testMerge <- subset(testMerge, select = -c(SellerG, Method, medianArea, medianYear, medianCar, medianYearType))
+
+# view if any null columns
+colSums(is.na(testMerge))
+
+# export to a cleaned csv file
+write.csv(testMerge, file="./melbourne_cleaned.csv")
--- melbourne-analysis.R
+++ melbourne-analysis.R
@@ -1,45 +1,21 @@
-library(tidyverse)
+library(lubridate)
 
-melbourne <- read.csv("melb_data_raw.csv", header=TRUE)
-glimpse(melbourne)
+melbourne <- read.csv("melbourne_cleaned.csv", header=TRUE)
 
-# We don't need method, seller, or address.
-melbourne <- subset(melbourne, select = -c(Address, SellerG, Method))
+# ==============================================================================
+# The dataset contains multiple categorical variables, so create a list of them
+# and tell R to turn them into a factor
+# ==============================================================================
 
-# telling R which predictors are categorical
-melbourne$Type <- factor(melbourne$Type)
-melbourne$Regionname <- factor(melbourne$Regionname)
-melbourne$Postcode <- factor(melbourne$Postcode)
-melbourne$CouncilArea <- factor(melbourne$CouncilArea)
-melbourne$Suburb <- factor(melbourne$Suburb)
+cats <- c('Type', 'Regionname', 'Postcode', 'CouncilArea', 'Suburb')
+melbourne[,cats] <- lapply(melbourne[,cats], factor)
 
-# telling R which predictor is a date column 
-melbourne$Date <- as.Date(melbourne$Date)  # - the dates are in different formats so we'll have to standardize the formatting
+# ===============================================================================
+# There are multiple date formats in the date column {17/09/2016, 4/03/2017, etc.}
+# We can standardize the date formatting with the lubridate package 
+# ===============================================================================
 
-# Checking for null columns
-colSums(is.na(melbourne))
-
-# We can drop the 62 instances where the cars predictor is NA
-melbourne <- melbourne[!is.na(melbourne$Car),]
-
-"Only building area and year built have NA values.
- We can two extra tables with year built, or building area
- dropped. Then filter the two tables to pull all rows where
- building area and year built contain values. We can then use the 
- non-null datasets to test each predictors significance separately.
- I suspect buildingArea will not be important because it's likely
- correlated to number of rooms, but we may have to do something about year built."
-
-yearBuiltAnalysis = na.omit(subset(melbourne, select = -c(BuildingArea)))
-buildingAreaAnalysis = na.omit(subset(melbourne, select = -c(YearBuilt)))
-
-
-yearBuiltResult <- lm(Price~Rooms+Type+Distance+Bedroom2+Bathroom+Car+Landsize+YearBuilt+CouncilArea+Regionname+Propertycount, data=yearBuiltAnalysis)
-summary(yearBuiltResult)
-# in the prescense of the other predictors, year built is significant in estimating the price of a house, so we should keep it, < 2e-16 ***
-
-buildingAreaResult <- lm(Price~Rooms+Type+Distance+Bedroom2+Bathroom+Car+Landsize+BuildingArea+CouncilArea+Regionname+Propertycount, data=buildingAreaAnalysis)
-summary(buildingAreaResult)
-# building area is less significant, but still significant, 0.012366 * 
-
-### NEXT STEPS: refine dataset via multicollinearity tests, outlier detection, possible transforms, etc.
+mdy <- mdy(melbourne$Date)
+dmy <- dmy(melbourne$Date)
+mdy[is.na(mdy)] <- dmy[is.na(mdy)]
+melbourne$Date <- mdy
